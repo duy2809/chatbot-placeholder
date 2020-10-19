28b7a5eb7f40773e4d029c9128c683138e47a44c
Duy Anh <duyanhvn1999@gmail.com>
2020-10-20 02:10:20 +0700

    Chatbot V1.0

--- screen/ChatScreen.js
+++ screen/ChatScreen.js
@@ -1,10 +1,92 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useCallback, useEffect } from "react";
+import { Alert, LogBox, StyleSheet, Text, View } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
+import firebase from "firebase";
 
-export default function ChatScreen() {
+var firebaseConfig = {
+  apiKey: "AIzaSyAHbhO19nd_wSN93fzYyJYwAnmlxhG1-dQ",
+  authDomain: "chatbot-placeholder.firebaseapp.com",
+  databaseURL: "https://chatbot-placeholder.firebaseio.com",
+  projectId: "chatbot-placeholder",
+  storageBucket: "chatbot-placeholder.appspot.com",
+  messagingSenderId: "281623463894",
+  appId: "1:281623463894:web:f4296fe7ce4d649873c677",
+};
+
+if (firebase.apps.length === 0) {
+  var firebase_init = firebase.initializeApp(firebaseConfig);
+  var messagesRef = firebase_init.database().ref("/messages");
+  var recordsRef = firebase_init.database().ref("/records");
+}
+
+LogBox.ignoreLogs(["Setting a timer for a long period of time"]);
+
+export default function ChatScreen({ route }) {
+  const [messages, setMessages] = useState([]);
+  const { username } = route.params;
+
+  const onSend = useCallback((messages = []) => {
+    let message_user = messages[0];
+    messagesRef.push(JSON.stringify(message_user));
+    let text_user = message_user["text"];
+    console.log("User: " + text_user);
+    fetch("http://df9b91727c4f.ngrok.io/predict", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messages[0]),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let text_bot = data["text"];
+        console.log("Bot: " + text_bot);
+        messagesRef.push(JSON.stringify(data));
+        recordsRef.push({
+          _user: text_user,
+          bot: text_bot,
+        });
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    messagesRef.on("value", (snapshot) => {
+      var arrMessages = [];
+      snapshot.forEach((childSnapshot) => {
+        //console.log("Doc du lieu cac child");
+        //var childKey = childSnapshot.key;
+        var childData = childSnapshot.val();
+        arrMessages.unshift(JSON.parse(childData));
+      });
+      arrMessages.push({
+        text: `Chào bạn ${username}, tôi là chatbot tư vấn trong lĩnh vực sức khỏe và dinh dưỡng và tôi được tạo ra bởi nhóm Placeholder. Bạn có thể hỏi bất cứ điều gì bạn đang quan tâm nhé! :>`,
+        user: {
+          _id: "2",
+          name: "Placeholder",
+          avatar: "https://image.flaticon.com/icons/png/512/2040/2040946.png",
+        },
+        _id: 1,
+      });
+      setMessages(arrMessages);
+    });
+    return () => {
+      messagesRef.off("value");
+    };
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>ChatScreen</Text>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     </View>
   );
 }
@@ -12,7 +94,7 @@ export default function ChatScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    backgroundColor: "white",
+    paddingHorizontal: 5,
   },
 });
--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -1,12 +1,39 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  Image,
+} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Input, Header } from "react-native-elements";
 
 export default function HomeScreen({ navigation }) {
+  const [username, setUsername] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Chat")}>
-        <Text>Next page</Text>
+      <Text style={styles.header}>Chatbot Placeholder</Text>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: "https://image.flaticon.com/icons/png/512/2040/2040946.png",
+        }}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => {
+          setUsername(text);
+        }}
+      />
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Chat", { username: username })}
+        style={[styles.btn, styles.shadowStyle]}
+      >
+        <Text style={{ fontSize: 20, color: "white" }}>Next</Text>
       </TouchableOpacity>
     </View>
   );
@@ -15,7 +42,48 @@ export default function HomeScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  header: {
+    fontSize: 35,
+    marginBottom: 20,
+    color: "#66C3FF",
+  },
+
+  btn: {
+    paddingVertical: 10,
+    paddingHorizontal: 125,
+    backgroundColor: "#66C3FF",
+    borderRadius: 10,
+    marginTop: 10,
+  },
+
+  shadowStyle: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  input: {
+    width: "70%",
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 10,
+  },
+
+  logo: {
+    width: 300,
+    height: 250,
+    marginBottom: 30,
   },
 });
